1447ea66e21d74eb00efdf2f2c6998ce7da656ef
wadodude <jerry.mathew.ce@mail.com>
2013-07-22 12:33:50 -0500

    Initial commit to github

---
+++ QuikTODO/PromptViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuikTODO
+{
+    public class PromptViewModel
+    {
+        public string PromptTitle { get; set; }
+        public string PromptMessage { get; set; }
+        public bool IsYesNoCancel { get { return PromptType == PromptType.YesNoCancel; } }
+        public bool IsYesNo { get { return PromptType == PromptType.YesNo; } }
+        public bool IsReminder { get { return PromptType == PromptType.SnoozeCancel; } }
+        public bool IsInformation { get { return PromptType == PromptType.Ok; } }
+        public bool IsOkCancel { get { return PromptType == PromptType.OkCancel; } }
+
+        public PromptType PromptType { get; set; }
+
+        public PromptViewModel(string title, string message, PromptType promptType)
+        {
+            PromptType = promptType;
+            PromptTitle = title;
+            PromptMessage = message;
+        }
+    }
+
+    public enum PromptType
+    {
+        YesNo = 1,
+        YesNoCancel =2,
+        Ok = 3,
+        OkCancel = 4,
+        SnoozeCancel = 5
+    }
+
+    public enum PromptResponse
+    {
+        NotSpecified = 0,
+        Ok = 1,
+        Yes = 2,
+        No = 3,
+        Snooze = 4,
+        Cancel = 5
+    }
+}
---
+++ QuikTODO/TimelineWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuikTODO
+{
+    /// <summary>
+    /// Interaction logic for TimelineWindow.xaml
+    /// </summary>
+    public partial class TimelineWindow : Window
+    {
+        TimelineViewModel vm = null;
+        public TimelineWindow(ObservableCollection<Task> tasks)
+        {
+            InitializeComponent();
+            //this.DataContext = new TimelineViewModel(new ObservableCollection<Task>(tasks.Where(i => i.ShowThisTask).ToList()));
+            this.DataContext = new TimelineViewModel(tasks);
+            vm = (TimelineViewModel)this.DataContext;
+        }
+
+        private void Slider_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            var width = MySlider.ActualWidth;
+            System.Windows.Controls.Slider slider = sender as System.Windows.Controls.Slider;
+            if (slider != null)
+            {
+                //var point = slider.TranslatePoint(Mouse.GetPosition(Application.Current.MainWindow), slider);
+                //var prompt = new PromptWindow("actual width", point.ToString(), PromptType.Ok);
+                //prompt.ShowDialog();
+                //prompt = null;
+                vm.SliderValue = (int)slider.Value;
+            }
+        }
+    }
+}
